f7a444c21ce99f9761c53531684c9b816a85f1aa
damia <damia@DESKTOP-G1NUIKK>
2021-12-15 22:15:05 +0100

    Add project files.

---
+++ L2_4_Zad_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L2_4_Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char variableFirst = 'A';
+            char variableSecond = 'B';
+            char variableThird = 'C';
+
+            Console.WriteLine($"{variableThird}, {variableSecond}, {variableFirst}");
+        }
+    }
+}
